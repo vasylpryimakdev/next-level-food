8098547a8b7c094ef0097197e40f80d17ee40321
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-22 14:41:12 +0200

    Set basic routes

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,3 @@
+export default function SingleMealPage() {
+  return <div>SingleMealPage</div>;
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,3 @@
+export default function CommunitySharePage() {
+  return <div>CommunitySharePage</div>;
+}
